1698952ea43c39fe3e9712ee975ff006a4f5ffe7
Divevin <108499842+Divevin@users.noreply.github.com>
2023-08-17 20:41:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,76 @@
-alert("VENDO PAMONHA")
+//variavel
+const nome = "Vinicius" //Constantes
+
+let sobrenome //Mais moderno - exige ser criada antes de usar
+
+var idade // Não exige ser criada antes de usar (nao da erro)
+
+//----------------------
+//COMANDOS CONDICIONAIS 
+/*
+if(CONDICAO){
+
+}
+*/
+
+let numero = 100
+if(numero > 50){
+
+}else if(numero <10){
+
+}else{
+
+}
+
+//IF TERNÁRIO 
+//CONDIÇÃO ? VERDADEIRO : FALSO
+
+numero>50 ? alert("Verdade"): alert("Mentira")
+
+//SWITCH CASE
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+
+//--------------------------------------------------
+//Laço de Repetição
+
+//FOR - Mais Organizado - Inicio e fim determinados  
+
+//for(VLR_INICIAL; Condição de Parada; Incremento){}
+
+for(let i=0; i<10; i++){
+    //codigos incríveis
+}
+
+
+//WHILE - Mais Maleável - Permite laços INFINITOS
+//WHILE(CONDIÇÃO){ Comando que provoca a parada }
+
+while(numero >10){
+
+    numero -- //ou ++ mesma coisa de numero = numero+1
+    numero -=10 //Mesma coisa de numero = numero -10
+
+}
+
+//DO... WHILE
+do {
+    
+} while (numero>10);
+
+//--------------------------------------
+//FUNCAO
+
+//function nome(parametros){codigos}
+function salvar(nome, idade){
+    console.log(`Nome: ${nome} - Idade ${Idade}`) //Isso e uma função void, sem retorno
+} //FIM DA FUNÇÃO
+
+salvar("Ronan", 30)
